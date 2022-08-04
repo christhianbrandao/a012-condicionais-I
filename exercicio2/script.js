f60cad87b267d2d40967b52e2aeecc6a343511a3
christhian brandão <christhianbrandao69@gmail.com>
2022-08-04 17:09:59 -0300

    fim

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,19 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
-
-if(){
-
+let idade = 21
+if(idade>=18){
+    console.log("você tem mais de 18 anos")
 }else{
-	
+    console.log('você não tem mais de 18 anos')
+}
+
+let terminouEnsinoMedio = true
+if(terminouEnsinoMedio === true){
+    console('você terminou o ensino medio')
+}else{
+    console.log("você ainda não terminou o ensino medio")
+}
+let isCursandoOutraFaculdade = false
+if( isCursandoOutraFaculdade===false){
+    console.log("você não esta cursando alguma faculdade")
+}else{
+    console.log("você esta cursando alguuma faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,20 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual a a sua nacionalidade?')
+
+if(nacionalidade === "brasileira"){
+    console.log("você nasceu no Brasil")
+
+}else if(nacionalidade === "argentina"){
+    console.log("argentina")
+    
+}else if(nacionalidade === "uruguaia"){
+    console.log("uruguaia")
+    
+}else if(nacionalidade === "chilena"){
+    console.log("chilena")
+
+}else if(nacionalidade === "colombiana"){
+    console.log("colombiana")
+
+}else{
+    console.log("nacionalidade não encontreda, tente novamente")
+}
